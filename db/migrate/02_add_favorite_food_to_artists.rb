d5debdc6beadd9369be5e4810122a3f00daa7b76
Savannah C <sgcorrell@gmail.com>
2019-05-29 21:07:07 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,4 +1,4 @@
-class AddFavoriteFoodToArtists < ActiveRecord::Migration
+class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.2]
   def change
     add_column :artists, :favorite_food, :string
   end
